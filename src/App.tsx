e34a607d1d473033c8d54699c3753241536d1605
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-12-09 19:51:29 -0300

    Add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './assets/styles/custom.scss'
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar></Navbar>
     <h1>DsCatalog</h1>
+    </>
   );
 }
 
